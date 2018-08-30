a85e328debfe04e1e86f70b741ce14347e07fa45
billo <philip.billaudelle@n26.com>
2018-08-30 10:12:50 +0200

    Remove plot4 from plot1.R

--- plot1.R
+++ plot1.R
@@ -19,30 +19,3 @@ hist(power_consumption_subset$Global_active_power, col = "red",
      xlab = "Global Active Power (kilowatts)",
      main = "Global Active Power")
 dev.off()
-
-# Make plot4 
-png("plot4.png", width = 480, height = 480)
-par(mfcol=c(2,2))
-        # Make plot 1/4
-plot(power_consumption_subset$date, power_consumption_subset$Global_active_power, type="n", 
-     xlab = "", ylab = "Global Active Power (kilowatts)")
-lines(power_consumption_subset$date, power_consumption_subset$Global_active_power, type = "l")
-
-        # Make plot 2/4
-plot(power_consumption_subset$date, power_consumption_subset$Sub_metering_1, type="l", 
-     xlab = "", ylab = "Energy sub metering", col = "black")
-lines(power_consumption_subset$date, power_consumption_subset$Sub_metering_2, 
-      type = "l", col = "red")
-lines(power_consumption_subset$date, power_consumption_subset$Sub_metering_3, 
-      type = "l", col = "blue")
-legend("topright", col = c("black", "red", "blue"), lty = c(1,1,1),
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.5, bty = "n")
-
-        # Make plot 3/4
-plot(power_consumption_subset$date, power_consumption_subset$Voltage, type = "l",
-     xlab = "datetime", ylab = "Voltage")
-
-        #Make plot 4/4
-plot(power_consumption_subset$date, power_consumption_subset$Global_reactive_power, type = "l",
-     xlab = "datetime", ylab = "Global_reactive_power")
-dev.off()
